6bf9c08e5d752bfbe6f3a0c3b9dd0ccb97aecb12
Jessi Ijelman Lipovetzky <46876160@est.ort.edu.ar>
2022-03-28 15:58:29 -0300

    Excercises 7 and 8 finisehed
    Excercises 7 and 8 finisehed
    Excercise 9 began.

---
+++ Assets/Ejercicio7.cs
@@ -0,0 +1,61 @@
+﻿//Realizá un programa que al ingresar por Inspector tres números enteros num1, num2 y num3, muestre el valor del mayor de todos.
+//En caso de igualdad entre los tres imprimir "Los números son iguales".
+//Test1: num1 = 12, num2 = 4, num3= 7. Resultado esperado: El mayor número es num1
+//Test2: num1 = 2, num2 = 65, num3= 8. Resultado esperado: El mayor número es num2
+//Test3: num1 = 3, num2 = 10, num3= 28. Resultado esperado: El mayor número es num3
+//Test4: num1 = 5, num2 = 5, num3= 5. Resultado esperado: Los números son iguales
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio7 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int n;
+    public int num1 = 1;
+    public int num2 = 2;
+    public int num3 = 3;
+
+    void Start()
+    {
+        if (num1 == num2 && num2 == num3)
+        {
+            Debug.Log("Son los 3 iguales.");
+        }
+
+        else
+        {
+            if (num1 > num2)
+            {
+                n = num1;
+
+                if (num3 > n)
+                {
+                    Debug.Log("El mayor es " + num3);
+                    n = num3;
+                }
+
+                else
+                {
+                    Debug.Log("El mayor es " + num1);
+                }
+            }
+
+            if (num2 > n)
+            {
+                n = num2;
+            }
+
+            Debug.Log("El mayor es " + n);
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Ejercicio9.cs
@@ -0,0 +1,31 @@
+﻿//Realizá un programa que resuelva el siguiente problema:
+//Tres personas aportan diferente capital a una sociedad. Se desea saber qué porcentaje del
+//total aportó cada una (indicando nombre y porcentaje) y cuál es el monto del total aportado por las tres.
+//Ingresar por Inspector el ingreso del capital aportado por cada una de las personas y luego
+//mostrar lo pedido en el siguiente formato:
+//Nombre:... capital aportado: $... , Porcentaje del capital: %... , Monto total aportado: $....
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio9 : MonoBehaviour
+{
+    int aporte1 = 1;
+    int aporte2 = 2;
+    int aporte3 = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int sumaaportes = aporte1 + aporte2 + aporte3;
+        int porcentaje1 = aporte1*100/sumaaportes;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
